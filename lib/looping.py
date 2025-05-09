0b88bdeeea873552b042bd2b48f5509076c88e10
Adrian-Amoke <adrian.amoke@student.moringaschool.com>
2025-05-10 00:10:41 +0300

    Done

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,31 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    return [pow(i,2) for i in int_list]
 
 def fizzbuzz():
     # code goes here!
-    pass
+    i = 0
+    for i in range(1,101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+        i += 1
+
+happy_new_year()
+square_integers([1, 2, 3, 4, 5])
+square_integers([-1,-2,-3,-4,-5])
+fizzbuzz()
